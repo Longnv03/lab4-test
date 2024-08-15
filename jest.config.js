21b422b4d28ef40b72a03eafa8a3d104da2552ff
LongAnh03 <s3926245@rmit.edu.vn>
2024-08-15 13:52:11 +1000

    Update Jest configuration to include test file patterns

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
     'default',
     ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }],
   ],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)',
+  ],
 };
